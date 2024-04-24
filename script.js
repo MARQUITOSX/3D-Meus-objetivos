c9200cc20a0737277e9d9c96f0e981dae1b104dc
MARQUITOSX <116809028+MARQUITOSX@users.noreply.github.com>
2024-04-24 22:46:53 +0000

    24/04 /24

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const botoes = document.querySelectorAll('.botao');
-for(let i=0;i <botoes.lenght;i++){
+const botoes = document.querySelectorAll('.aba_conteudo');
+
+for(let i=0;i < botoes.length; i++){
   botoes[i].onclick = function() {
-  for(let k=0; k < botoes.lenght; k++) {
-    botoes [k].classList.remove('ativo') }
-  botoes[i].classList.add('ativo'); 
+    for(let k=0; k < botoes.length; k++) {
+      botoes[k].classList.remove('ativo');
+      textos[k].classList.remove('ativo');
+    }
+  botoes[i].classList.add('ativo');
+  textos[i].classList.add('ativo');
   }
 }
+
+// iniciar os contadores
+const.contadores = document.querySelectorAll('.contador');
+
+let dataHoje = new Date();
+contadores[0].textContent(dataHoje);
